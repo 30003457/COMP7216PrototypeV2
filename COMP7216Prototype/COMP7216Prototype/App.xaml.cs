bafaf768e4617f93d69f1db4747ec6efe8f60107
Lewis Evans <27033957@student.toiohomai.ac.nz>
2020-11-12 13:54:18 +1300

    Login Database

--- COMP7216Prototype/COMP7216Prototype/App.xaml.cs
+++ COMP7216Prototype/COMP7216Prototype/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using COMP7216Prototype.View;
+﻿
+using COMP7216Prototype.View;
 using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,11 +9,24 @@ namespace COMP7216Prototype
 {
     public partial class App : Application
     {
+        static SQLiteHelper db;
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new LoginPage());
+        }
+
+        public  static SQLiteHelper SQLiteDb
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new SQLiteHelper(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "XamarinSQLite.db3"));
+                }
+                return db;
+            }
         }
 
         protected override void OnStart()
